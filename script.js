0f8d197fd208a88216f29fcffe4c09d30c835b21
Glitch (hello-webpage) <none>
2019-01-19 10:09:26 +0000

    🏙☔️ Checkpoint ./script.js:2849330/359 ./index.html:2849330/44 ./style.css:2849330/18

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-window.addEventListener('load', init); 
+window.addEventListener('load', init)
 let scene
 let camera
 let renderer
+let sceneObject
 
 
 function init() {
-  scene = new THREE.Scene(); 
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
 }
 
+function addCube() {
+  
+}
