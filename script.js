c2982f9e633ae7d72f2aa4e83ba14a198f632b7e
Glitch (hello-webpage) <none>
2019-01-19 10:20:26 +0000

    🌥👻 Checkpoint ./script.js:2849330/1216 ./index.html:2849330/20

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ let sceneObject
 function init() {
   scene = new THREE.Scene()
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.z = 50
+  
   renderer = new THREE.WebGLRenderer()
   renderer.setSize(window.innerWidth, window.innerHeight)
+  
   document.body.appendChild(renderer.domElement)
+  addCube()
 }
 
 function addCube() {
+  let material = new THREE.BoxGeometry(100, 100, 100)
+  let geometry = new THREE.MeshBasicMaterial()
+  let mesh = new THREE.Mesh(geometry, material)
+  mesh.position.z = -5
+  scene.add(mesh)
   
+  console.log(scene)
 }
