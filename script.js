e3115267d593b82cdfc704de772263c82b55bc56
Glitch (hello-webpage) <none>
2019-01-19 10:30:38 +0000

    🍐🐜 Checkpoint ./script.js:2849330/331 ./style.css:2849330/40

--- script.js
+++ script.js
@@ -2,27 +2,32 @@ window.addEventListener('load', init)
 let scene
 let camera
 let renderer
-let sceneObject
+let mesh
 
 
 function init() {
   scene = new THREE.Scene()
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  camera.position.z = 50
+  camera.position.z = 5
   
   renderer = new THREE.WebGLRenderer()
   renderer.setSize(window.innerWidth, window.innerHeight)
   
   document.body.appendChild(renderer.domElement)
   addCube()
+  animationLoop()
 }
 
 function addCube() {
   let material = new THREE.BoxGeometry(100, 100, 100)
   let geometry = new THREE.MeshBasicMaterial()
-  let mesh = new THREE.Mesh(geometry, material)
-  mesh.position.z = -5
+  mesh = new THREE.Mesh(geometry, material)
   scene.add(mesh)
+}
+
+function animationLoop() {
   
-  console.log(scene)
+  renderer.render(scene, camera)
+  
+  requestAnimationFrame(() => { animationLoop() })
 }
