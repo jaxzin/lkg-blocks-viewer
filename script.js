e2a45c77e5b5862e5d959ebbb4e3d22664ac93fd
Glitch (hello-webpage) <none>
2023-12-16 07:23:47 +0000

    🍯🏥 Checkpoint ./script.js:61675861/4276

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // globals shared between the two main event listeners
 let camera;
 let renderer;
-let atmosphereMaterial;
+let plane;
+let shaderMaterial;
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
 // Setup the scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -32,29 +33,38 @@ const vertexShader = `
 const fragmentShader = `
     uniform sampler2D uTexture;
     uniform vec2 uTextureSize;
-    uniform float uViewAngle;
+    uniform float uRelativeAngle; // Relative angle between camera and object
 
     void main() {
-        // Adjust for an 8x12 grid and 0.75 aspect ratio
-        float angleStep = 360.0 / 96.0; // Assuming you have 96 images (8x12)
-        float index = floor(uViewAngle / angleStep);
-        float row = floor(index / 12.0); // 12 columns
-        float col = mod(index, 12.0); // 8 rows
-        vec2 cellSize = vec2(1.0 / 12.0, (0.75 / 12.0));
-        vec2 cellOffset = vec2(col / 12.0, row / 8.0);
-        vec2 uv = (gl_FragCoord.xy / uTextureSize) * cellSize + cellOffset;
+        // Define the viewing angle range (in radians)
+        float maxAngle = radians(45.0); // 90 degrees range (45 degrees on either side)
 
-        gl_FragColor = texture2D(uTexture, uv);
+        // Check if the relative angle is within the range
+        if (abs(uRelativeAngle) > maxAngle) {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0); // Fade to black
+        } else {
+            // Rest of your shader logic for selecting the correct image
+            float angleStep = 360.0 / 96.0;
+            float index = floor((uRelativeAngle + maxAngle) / angleStep * (96.0 / (2.0 * maxAngle)));
+            float row = floor(index / 12.0);
+            float col = mod(index, 12.0);
+            vec2 cellSize = vec2(1.0 / 12.0, (0.75 / 12.0));
+            vec2 cellOffset = vec2(col / 8.0, row / 12.0);
+            vec2 uv = (gl_FragCoord.xy / uTextureSize) * cellSize + cellOffset;
+
+            gl_FragColor = texture2D(uTexture, uv);
+        }
     }
+
 
 `;
 
 // Create a ShaderMaterial
-const shaderMaterial = new THREE.ShaderMaterial({
+shaderMaterial = new THREE.ShaderMaterial({
     uniforms: {
         uTexture: { value: textureAtlas },
         uTextureSize: { value: new THREE.Vector2(window.innerWidth, window.innerHeight * 0.75) }, // Adjusted for 0.75 aspect ratio
-        uViewAngle: { value: 0.0 }
+        uRelativeAngle: { value: 0.0 }
     },
     vertexShader,
     fragmentShader
@@ -62,27 +72,42 @@ const shaderMaterial = new THREE.ShaderMaterial({
 
 // Add a mesh using the shader material
 const geometry = new THREE.PlaneGeometry(5, 5);
-const plane = new THREE.Mesh(geometry, shaderMaterial);
+plane = new THREE.Mesh(geometry, shaderMaterial);
 scene.add(plane);
 
 camera.position.z = 5;
 
-// Update view angle based on camera rotation
-function updateViewAngle() {
-    // Simplified example; adjust this to calculate the actual angle
-    shaderMaterial.uniforms.uViewAngle.value = camera.rotation.y * (180 / Math.PI);
+// Function to calculate the relative angle between camera and object
+function calculateRelativeAngle(camera, object) {
+    let cameraDirection = new THREE.Vector3();
+    camera.getWorldDirection(cameraDirection);
+    let objectDirection = new THREE.Vector3(0, 0, -1); // Assuming the object's front is along negative Z-axis
+    objectDirection.applyQuaternion(object.quaternion);
+
+    return cameraDirection.angleTo(objectDirection) - Math.PI / 2;
+}
+  
+// Function for oscillating rotation
+function oscillateRotation(time, amplitude, period) {
+    // Oscillate between -amplitude and +amplitude over a given period
+    return amplitude * Math.sin(time / period);
 }
 
 // Animation loop
-function animate() {
+function animate(time) {
     requestAnimationFrame(animate);
 
-    updateViewAngle();
+    // Rotate the plane back and forth by 45 degrees
+    let angle = oscillateRotation(time, Math.PI / 4, 2000); // 45 degrees, 2000 ms period
+    plane.rotation.y = angle;
+
+    // Update relative angle uniform
+    //shaderMaterial.uniforms.uRelativeAngle.value = calculateRelativeAngle(camera, plane);
 
     renderer.render(scene, camera);
 }
 
-animate();
+animate(0);
 
 });
 
