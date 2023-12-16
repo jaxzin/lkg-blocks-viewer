3b7946d54329ac4545dcec0fa5622d9bcfb85c1a
Glitch (hello-webpage) <none>
2023-12-16 07:35:48 +0000

    🐋🚤 Checkpoint ./script.js:61675861/3379

--- script.js
+++ script.js
@@ -41,14 +41,19 @@ const fragmentShader = `
 
         // Check if the relative angle is within the range
         if (abs(uRelativeAngle) > maxAngle) {
-            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0); // Fade to black
+            // Determine if we are looking at the front or back of the plane
+            if (uRelativeAngle > 0.0) {
+                gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Front: Red
+            } else {
+                gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); // Back: White
+            }
         } else {
             // Rest of your shader logic for selecting the correct image
-            float angleStep = 360.0 / 96.0;
+            float angleStep = 2.0 * maxAngle / radians(96.0);
             float index = floor((uRelativeAngle + maxAngle) / angleStep * (96.0 / (2.0 * maxAngle)));
-            float row = floor(index / 12.0);
-            float col = mod(index, 12.0);
-            vec2 cellSize = vec2(1.0 / 12.0, (0.75 / 12.0));
+            float row = floor(index / 8.0);
+            float col = mod(index, 8.0);
+            vec2 cellSize = vec2(1.0 / 8.0, (0.75 / 12.0));
             vec2 cellOffset = vec2(col / 8.0, row / 12.0);
             vec2 uv = (gl_FragCoord.xy / uTextureSize) * cellSize + cellOffset;
 
@@ -77,32 +82,60 @@ scene.add(plane);
 
 camera.position.z = 5;
 
-// Function to calculate the relative angle between camera and object
 function calculateRelativeAngle(camera, object) {
+    // Camera direction in world space
     let cameraDirection = new THREE.Vector3();
     camera.getWorldDirection(cameraDirection);
-    let objectDirection = new THREE.Vector3(0, 0, -1); // Assuming the object's front is along negative Z-axis
-    objectDirection.applyQuaternion(object.quaternion);
 
-    return cameraDirection.angleTo(objectDirection) - Math.PI / 2;
+    // Object's front direction. Assuming the front is along negative Z-axis in object's local space.
+    let objectDirection = new THREE.Vector3(0, 0, -1);
+    objectDirection.applyQuaternion(object.quaternion); // Convert to world space
+
+    // Normalize directions to ensure they are unit vectors
+    cameraDirection.normalize();
+    objectDirection.normalize();
+
+    // Calculate the dot product
+    let dot = cameraDirection.dot(objectDirection);
+
+    // Clamp value to avoid any potential numerical issues and get the angle
+    dot = THREE.MathUtils.clamp(dot, -1.0, 1.0);
+    let angle = Math.acos(dot);
+
+    // Convert angle to range [-PI/2, PI/2] as we are interested in -90 to 90 degrees
+    if (angle > Math.PI / 2) {
+        angle = Math.PI - angle;
+    }
+
+    // Return angle in radians
+    return angle - Math.PI / 2;
 }
+
   
 // Function for oscillating rotation
 function oscillateRotation(time, amplitude, period) {
     // Oscillate between -amplitude and +amplitude over a given period
     return amplitude * Math.sin(time / period);
 }
+  
+const controls = 
+      new OrbitControls( camera, renderer.domElement );
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0.5;  
 
 // Animation loop
 function animate(time) {
     requestAnimationFrame(animate);
 
+    // auto-rotate the camera
+    controls.update();  
+  
     // Rotate the plane back and forth by 45 degrees
-    let angle = oscillateRotation(time, Math.PI / 4, 2000); // 45 degrees, 2000 ms period
-    plane.rotation.y = angle;
+    // let angle = oscillateRotation(time, Math.PI / 4, 2000); // 45 degrees, 2000 ms period
+    // plane.rotation.y = angle;
 
     // Update relative angle uniform
-    //shaderMaterial.uniforms.uRelativeAngle.value = calculateRelativeAngle(camera, plane);
+    shaderMaterial.uniforms.uRelativeAngle.value = calculateRelativeAngle(camera, plane);
 
     renderer.render(scene, camera);
 }
