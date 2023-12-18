725dd06e8068267fbd24b571f39376a8edd49cd2
Glitch (hello-webpage) <none>
2023-12-18 02:15:25 +0000

    🐗📻 Checkpoint ./script.js:61675861/12617

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ import { BlockCard } from './BlockCard.js';
 // globals shared between the two main event listeners
 let camera;
 let renderer;
-let quiltViewer;
-let quiltViewerMaterial;
 let quiltTexture;
 let planeGrabbed = false;
 let grabbedController = null;
@@ -19,11 +17,8 @@ let hand1, hand2;
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
 
-let room, marker, floor, baseReferenceSpace, raycaster, group;
-
-let INTERSECTION;
-const tempMatrix = new THREE.Matrix4();
-
+let room, marker, floor, baseReferenceSpace, raycaster; 
+let cardGroup;
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
@@ -50,6 +45,8 @@ document.body.appendChild(VRButton.createButton(renderer));
 
 renderer.shadowMap.enabled = true;
 
+// In VR mode, show a basic floor and walls 
+//   since a black void can be a little disorienting
 room = new THREE.LineSegments(
   new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ).translate( 0, 3, 0 ),
   new THREE.LineBasicMaterial( { color: 0xbcbcbc } )
@@ -57,25 +54,19 @@ room = new THREE.LineSegments(
 room.visible = false;
 scene.add( room );
 
-marker = new THREE.Mesh(
-  new THREE.CircleGeometry( 0.25, 32 ).rotateX( - Math.PI / 2 ),
-  new THREE.MeshBasicMaterial( { color: 0xbcbcbc } )
-);
-marker.visible = false;
-scene.add( marker );
-  
 raycaster = new THREE.Raycaster();
   
-  
-group = new THREE.Group();
-scene.add( group );
+
+// This is a collection of all the cards in the scene
+cardGroup = new THREE.Group();
+scene.add( cardGroup );
   
 function onSelectStart(event) {
 
   this.userData.isSelecting = true;
   
   const controller = event.target;
-  const object = group.children[0];
+  const object = cardGroup.children[0];
   controller.attach( object );
   controller.userData.selected = object;
   object.children[0].material.emissive.b = 1;
@@ -91,38 +82,16 @@ function onSelectEnd(event) {
   if( controller.userData.selected !== undefined ) {
     const object = controller.userData.selected;
     object.children[0].material.emissive.b = 0;
-    group.attach( object );
+    cardGroup.attach( object );
     
     controller.userData.selected = undefined;
   }
 }  
   
-  
-function onSelectStart2() {
-
-  this.userData.isSelecting = true;
-
-}
-
-function onSelectEnd2() {
-
-  this.userData.isSelecting = false;
-
-  if ( INTERSECTION ) {
-
-    const offsetPosition = { x: - INTERSECTION.x, y: - INTERSECTION.y, z: - INTERSECTION.z, w: 1 };
-    const offsetRotation = new THREE.Quaternion();
-    const transform = new XRRigidTransform( offsetPosition, offsetRotation );
-    const teleportSpaceOffset = baseReferenceSpace.getOffsetReferenceSpace( transform );
-
-    renderer.xr.setReferenceSpace( teleportSpaceOffset );
-
-  }
-
-}  
+ 
   
 function onSessionStart() {
-    let card = group.children[0];
+    let card = cardGroup.children[0];
     card.position.set( 0, 1, -.5 );
     card.scale.set(.05,.05,.05);
     floor.visible = true;
@@ -185,7 +154,7 @@ function onSessionStart() {
 }
 
 function onSessionEnd() {
-  let card = group.children[0];
+  let card = cardGroup.children[0];
   
   // Clean up when the VR session ends
   card.position.set(0,0,-5);
@@ -200,32 +169,6 @@ renderer.xr.addEventListener('sessionstart', onSessionStart);
 renderer.xr.addEventListener('sessionend', onSessionEnd);
 
 
-function buildController( data ) {
-
-  let geometry, material;
-
-  switch ( data.targetRayMode ) {
-
-    case 'tracked-pointer':
-
-      geometry = new THREE.BufferGeometry();
-      geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 1 ], 3 ) );
-      geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( [ 0.5, 0.5, 0.5, 0, 0, 0 ], 3 ) );
-
-      material = new THREE.LineBasicMaterial( { vertexColors: true, blending: THREE.AdditiveBlending } );
-
-      return new THREE.Line( geometry, material );
-
-    case 'gaze':
-
-      geometry = new THREE.RingGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
-      material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
-      return new THREE.Mesh( geometry, material );
-
-  }
-
-}  
-  
 floor = new THREE.Mesh(
   new THREE.PlaneGeometry( 4.8, 4.8, 2, 2 ).rotateX( - Math.PI / 2 ),
   new THREE.MeshBasicMaterial( { color: 0xbcbcbc, transparent: true, opacity: 0.25 } )
@@ -256,265 +199,28 @@ const quiltDims = new THREE.Vector2(8, 12); // quilt col & row count
 const quiltRes = new THREE.Vector2(6400.0, 7462.0);
 
 const maxViewingAngle = 58.; // max viewing angle image (degrees)
-  
-// Define vertex shader
-const vertexShader = `
-    varying vec2 vUv;
 
-    void main() {
-        vUv = uv; // Assign the UV coordinates from the vertex attributes
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-`;
-
-// Define fragment shader
-const fragmentShader2 = `
-    uniform sampler2D uTexture;
-    uniform vec2 uTextureSize;
-    uniform float uRelativeAngle; // Relative angle between camera and object
-    uniform vec2 quiltDims;
-    uniform float viewCone;
-    varying vec2 vUv;
-
-    void main() {
-        // Define the viewing angle range (in radians)
-        float maxAngle = radians(viewCone * .5);
-        
-        // Normalize the angle to be within [0, 1]
-        float normalizedAngle = (maxAngle - uRelativeAngle) / (2.0 * maxAngle);
-
-        // Calculate the index
-        float cols = quiltDims.x;
-        float rows = quiltDims.y;
-        float totalImages = float(rows * cols); // Total number of images in the quilt
-        float index = floor(normalizedAngle * totalImages);
-
-        // Ensure index is within bounds
-        index = clamp(index, 0.0, totalImages - 1.0);
-
-        float row = floor(index / cols);
-        float col = mod(index, cols);
-
-        // Calculate cell size in UV space
-        vec2 cellSize = vec2(1. / cols, 1. / rows);
-        vec2 cellOffset = vec2(col / cols, row / rows);
-
-        // Calculate UV coordinates
-        vec2 cellUv = vUv * cellSize + cellOffset;
-
-        // Get the color from the texture
-        vec4 textureColor = texture2D(uTexture, cellUv);
-
-        // Calculate fade factor (1 at the edges of the cone, 0 at the center)
-        float fadeFactor = clamp(pow(abs(uRelativeAngle) / maxAngle, 5.), 0., 1.);
-
-        vec2 centerCellOffset = vec2(0.5, 0.5);
-        vec2 centerCellUv = vUv * cellSize + centerCellOffset;
-        vec4 standin = texture2D(uTexture, vUv * cellSize + centerCellOffset);
-        // Fade the off-axis stand-in to about 50% brightness
-        standin = mix(standin, vec4(0.,0.,0.,1.), 0.5);
-        
-        // Fade to the standin as the viewing angle approaches the bounds of the viewing cone
-        gl_FragColor = mix(textureColor, standin, fadeFactor);
-    }
-
-
-`;
-
-const fragmentShader = `
-    uniform sampler2D uTexture;
-    uniform vec2 uTextureSize;
-    uniform float uRelativeAngle; // Relative angle between camera and object
-    uniform vec2 quiltDims;
-    uniform float viewCone;
-    varying vec2 vUv;
-
-    void main() {
-        // Define the viewing angle range (in radians)
-        float maxAngle = radians(viewCone * .5);
-        
-        // Normalize the angle to be within [0, 1]
-        float normalizedAngle = (maxAngle - uRelativeAngle) / (2.0 * maxAngle);
-
-        // Calculate the index
-        float totalImages = float(quiltDims.x * quiltDims.y); // Total number of images in the quilt
-
-        // Calculate cell size in UV space
-        vec2 cellSize = 1. / quiltDims;
-        
-        // Calculate the normalized angle and use fract() to get the fractional part for interpolation
-        float index = fract(normalizedAngle * totalImages);
-
-        // Determine the current cell's index and the next cell's index
-        float currentCellIndex = floor(normalizedAngle * totalImages);
-        float nextCellIndex = currentCellIndex + 1.0;
-
-        // Ensure the cell index is within bounds
-        currentCellIndex = clamp(currentCellIndex, 0.0, totalImages - 1.0);
-        nextCellIndex = clamp(nextCellIndex, 0.0, totalImages - 1.0);
-
-        // Calculate the row and column for the current and next cells
-        vec2 currentCell = vec2(mod(currentCellIndex, quiltDims.x), floor(currentCellIndex / quiltDims.x));
-        vec2 nextCell = vec2(mod(nextCellIndex, quiltDims.x), floor(nextCellIndex / quiltDims.x));
-
-        // Calculate UV coordinates for the current and next cells
-        vec2 currentCellUv = (vUv * cellSize) + (currentCell / quiltDims);
-        vec2 nextCellUv = (vUv * cellSize) + (nextCell / quiltDims);
-
-        // Fetch the colors from the current and next cells
-        vec4 currentColor = texture2D(uTexture, currentCellUv);
-        vec4 nextColor = texture2D(uTexture, nextCellUv);
-
-        // Interpolate between the current and next cell based on the fractional index
-        vec4 textureColor = mix(currentColor, nextColor, index);
-        
-        // Calculate fade factor (1 at the edges of the cone, 0 at the center)
-        float fadeFactor = clamp(pow(abs(uRelativeAngle) / maxAngle, 5.), 0., 1.);
-
-        // Fade the off-axis stand-in to about 50% brightness
-        vec4 standin = mix(textureColor, vec4(0.,0.,0.,1.), 0.5);
-        
-        // Fade to the standin as the viewing angle approaches the bounds of the viewing cone
-        gl_FragColor = mix(textureColor, standin, fadeFactor);
-    }
-
-`;
-  
-// Create a ShaderMaterial
-quiltViewerMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-        uTexture: { value: quiltTexture },
-        uTextureSize: { value: quiltRes }, 
-        uRelativeAngle: { value: 0.0 },
-        quiltDims: { value: quiltDims }, 
-        viewCone: {value: maxViewingAngle} // viewing cone on the Looking Glass Portrait is 58º 
-    },
-    vertexShader,
-    fragmentShader,
-    side: THREE.DoubleSide // Make the material double-sided
-});
-
-// Function to create a rounded rectangle shape
-function createRoundedRectShape(width, height, radius) {
-    var shape = new THREE.Shape();
-    
-    // Calculate the offsets
-    var offsetX = -width / 2;
-    var offsetY = -height / 2;
-
-    // Starting point
-    shape.moveTo(offsetX, offsetY + radius);
-
-    // Top line and top-right corner
-    shape.lineTo(offsetX, offsetY + height - radius);
-    shape.quadraticCurveTo(offsetX, offsetY + height, offsetX + radius, offsetY + height);
-
-    // Right line and bottom-right corner
-    shape.lineTo(offsetX + width - radius, offsetY + height);
-    shape.quadraticCurveTo(offsetX + width, offsetY + height, offsetX + width, offsetY + height - radius);
-
-    // Bottom line and bottom-left corner
-    shape.lineTo(offsetX + width, offsetY + radius);
-    shape.quadraticCurveTo(offsetX + width, offsetY, offsetX + width - radius, offsetY);
-
-    // Left line and top-left corner
-    shape.lineTo(offsetX + radius, offsetY);
-    shape.quadraticCurveTo(offsetX, offsetY, offsetX, offsetY + radius);
-
-    return shape;
-}  
-function createRoundedRectGeometry(width, height, radius) {
-  const shape = createRoundedRectShape(width, height, radius);
-  const geometry = new THREE.ShapeGeometry(shape);  
-
-  // If the geometry is not already a BufferGeometry, convert it
-  if (!(geometry instanceof THREE.BufferGeometry)) {
-      geometry = new THREE.BufferGeometry().fromGeometry(geometry);
-  }
-
-  // Access the UV attribute
-  var uvs = geometry.attributes.uv;
-  for (let i = 0; i < uvs.array.length; i += 2) {
-      // Scale UVs
-      uvs.array[i] /= width;      // u coordinate
-      uvs.array[i + 1] /= height; // v coordinate
-
-      // Center the UVs
-      uvs.array[i] += 0.5;       // u coordinate
-      uvs.array[i + 1] += 0.5    // v coordinate
-
-  }
-
-  // Update the UV attribute
-  uvs.needsUpdate = true;  
-  return geometry;
-}
-
-const width = 3, height = 4, radius = 0.2;
-const quiltViewerGeometry = createRoundedRectGeometry(width, height, radius);
-  
-// Add a mesh using the shader material
-quiltViewer = new THREE.Mesh(quiltViewerGeometry, quiltViewerMaterial);
-quiltViewer.castShadow = true;
-quiltViewer.onBeforeRender = function( renderer, scene, camera, geometry, material, group ) {
-  // Update the viewing angle so the quilt viewer shader knows which quilt cell(s) to display
-  material.uniforms.uRelativeAngle.value = calculateRelativeAngle(camera, this);
-};
-  
+const cardWidth = 3, cardHeight = 4, cardCornerRadius = 0.2;
 const borderWidth = 0.25
-const borderGeometry = createRoundedRectGeometry(width+borderWidth, height+borderWidth, borderWidth);
-const border = new THREE.Mesh(borderGeometry, new THREE.MeshPhongMaterial({color: "#AAAAFF", side: THREE.DoubleSide}));
-border.position.z = -0.001;
-quiltViewer.add( border );
   
-const blockCard = new BlockCard(quiltTexture, width, height, radius, borderWidth, quiltDims, quiltRes, maxViewingAngle);
-group.add( blockCard );
-//group.add( quiltViewer );
-  
-//quiltViewer.position.set(0,0,-5);
+const blockCard = new BlockCard(
+  quiltTexture, 
+  cardWidth, 
+  cardHeight, 
+  cardCornerRadius, 
+  borderWidth, 
+  quiltDims, 
+  quiltRes, 
+  maxViewingAngle
+);
+cardGroup.add( blockCard );
 blockCard.position.set(0,0,-5);
-
-  
-function calculateRelativeAngle(camera, object) {
-  // Get the world space position of the object
-  let objectWorldPosition = new THREE.Vector3();
-  object.getWorldPosition(objectWorldPosition);
-  
-  // Calculate the direction from the object to the camera in world space
-  let toCameraDirection = new THREE.Vector3().subVectors(camera.position, objectWorldPosition);
-  toCameraDirection.y = 0; // Project onto the XZ plane by zeroing the Y component
-  toCameraDirection.normalize(); // Re-normalize the vector
-
-  // Get the forward normal of the object in world space
-  let objectNormal = new THREE.Vector3(0, 0, 1); // Default forward in local space
-  let objectWorldQuaternion = new THREE.Quaternion();
-  object.getWorldQuaternion(objectWorldQuaternion);
-  objectNormal.applyQuaternion(objectWorldQuaternion);
-  objectNormal.y = 0; // Project onto the XZ plane
-  objectNormal.normalize(); // Re-normalize the vector
-
-  // Calculate the dot product
-  let dot = toCameraDirection.dot(objectNormal);
-
-  // Calculate the angle
-  let angle = Math.acos(THREE.MathUtils.clamp(dot, -1.0, 1.0));
-
-  // Determine the sign of the angle based on the cross product
-  let cross = new THREE.Vector3().crossVectors(toCameraDirection, objectNormal);
-  if (cross.y < 0) {
-      angle = -angle;
-  }
-
-  // The angle is now between -PI (facing away) and PI (facing towards)
-  return angle;
-}
-
   
 const controls = 
       new OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true;
 controls.dampingFactor = 0.025;
-controls.target.copy(group.children[0].position);
+controls.target.copy(cardGroup.children[0].position);
 controls.update();
 
 // Lock rotation around the X axis
@@ -528,49 +234,9 @@ controls.minAzimuthAngle = - halfAngleLimit;
 controls.maxAzimuthAngle = halfAngleLimit;
 
   
-function intersectController() {
-  INTERSECTION = undefined;
-
-  if ( controller1 && controller1.userData.isSelecting === true ) {
-
-    tempMatrix.identity().extractRotation( controller1.matrixWorld );
-
-    raycaster.ray.origin.setFromMatrixPosition( controller1.matrixWorld );
-    raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
-
-    const intersects = raycaster.intersectObjects( [ floor ] );
-
-    if ( intersects.length > 0 ) {
-
-      INTERSECTION = intersects[ 0 ].point;
-
-    }
-
-  } else if ( controller2 && controller2.userData.isSelecting === true ) {
-
-    tempMatrix.identity().extractRotation( controller2.matrixWorld );
-
-    raycaster.ray.origin.setFromMatrixPosition( controller2.matrixWorld );
-    raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
-
-    const intersects = raycaster.intersectObjects( [ floor ] );
-
-    if ( intersects.length > 0 ) {
-
-      INTERSECTION = intersects[ 0 ].point;
-
-    }
-
-  }
-
-  if ( INTERSECTION ) marker.position.copy( INTERSECTION );
-
-  marker.visible = INTERSECTION !== undefined;
-}
-  
 function animate(timestamp, frame) {
     if (renderer.xr.isPresenting) {
-      intersectController();
+      //intersectController();
     } else {
       controls.update();
     }
