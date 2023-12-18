95e1e1050d41e5d5595ce5e441e9a97484144ab2
Glitch (hello-webpage) <none>
2023-12-18 05:49:19 +0000

    🐃🎟 Checkpoint ./script.js:61675861/13339 ./XRScene.js:61675861/6199

--- XRScene.js
+++ XRScene.js
@@ -1,50 +1,14 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { VRButton } from "three/addons/webxr/VRButton.js";
-import { XRControllerModelFactory } from "three/addons/webxr/XRControllerModelFactory.js";
-import { XRHandModelFactory } from "three/addons/webxr/XRHandModelFactory.js";
-import { BoxLineGeometry } from "three/addons/geometries/BoxLineGeometry.js";
+import * as THREE from 'three';
 
-import { BlockCard } from "./BlockCard.js";
-
-// globals shared between the two main event listeners
-let camera;
-let renderer;
-let quiltTexture;
-let planeGrabbed = false;
-let grabbedController = null;
-let hand1, hand2;
-let controller1, controller2;
-let controllerGrip1, controllerGrip2;
-
-let xrEnvironment, room, marker, floor, baseReferenceSpace;
-let cardGroup;
-
-let raycaster;
-
-const intersected = [];
-const tempMatrix = new THREE.Matrix4();
-
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  // Setup the scene, camera, and renderer
-  const scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.shadowMap.enabled = true;
-  document.body.appendChild(renderer.domElement);
-
-  // This is a collection of all the cards in the scene
-  cardGroup = new THREE.Group();
-  scene.add(cardGroup);
+// Responsible for setting up the VR/XR specific bits
+class XRScene {
   
+  setBaseReferenceSpace(ref) {
+    this.baseReferenceSpace = ref;
+  }
+  
+  constructor(renderer, document) {
+
   // Turn on WebXR support
   let referenceSpaceType = "local-floor"; // or 'local', 'unbounded', etc.
   let xrSession;
@@ -52,8 +16,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
   renderer.xr.enabled = true;
   renderer.xr.addEventListener("sessionstart", (event) => {
     xrSession = renderer.xr.getSession();
-    xrSession.requestReferenceSpace(referenceSpaceType).then((refSpace) => {
-      baseReferenceSpace = refSpace;
+    xrSession.
+      requestReferenceSpace(referenceSpaceType).
+      then(this.setBaseReferenceSpace().bind(this)); // TODO, I don't think this is going to bind to the right 'this'
     });
   });
   document.body.appendChild(VRButton.createButton(renderer));
@@ -251,131 +216,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
   );
   floor.receiveShadow = true;
   xrEnvironment.add(floor);
-
-  scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
-
-  const light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(0, 200, 0); // MODIFIED SIZE OF SCENE AND SHADOW
-  light.castShadow = true;
-  light.shadow.camera.top = 200; // MODIFIED FOR LARGER SCENE
-  light.shadow.camera.bottom = -200; // MODIFIED FOR LARGER SCENE
-  light.shadow.camera.right = 200; // MODIFIED FOR LARGER SCENE
-  light.shadow.camera.left = -200; // MODIFIED FOR LARGER SCENE
-  light.shadow.mapSize.set(4096, 4096);
-  scene.add(light);
-
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
-  scene.add(ambientLight);
-
-  
-  
-  // Setup some shared properties of all cards
-  const maxViewingAngle = 58; // max viewing angle image (degrees)
-
-  const cardWidth = .15,
-    cardHeight = .2,
-    cardCornerRadius = 0.01;
-  const borderWidth = 0.0125;
-  const borderColor = "#AAAAFF";
-  
-  
-  function createBlockCard(textureUrl, quiltDims) {
-    // Load the texture quilt
-    const textureLoader = new THREE.TextureLoader();
-    let quiltTexture = textureLoader.load(textureUrl);
     
-    let blockCard = new BlockCard(
-      quiltTexture,
-      cardWidth,
-      cardHeight,
-      cardCornerRadius,
-      borderWidth,
-      borderColor,
-      quiltDims,
-      maxViewingAngle
-    );
-
-    blockCard.castShadow = true;
-    blockCard.receiveShadow = true;
-    return blockCard;
   }
-  
-  // Our Christmas Tree 2023
-  let blockCard1 = createBlockCard(
-    'https://cdn.glitch.global/98b2b4e8-ce2c-4c4f-8e0c-3e762cb48276/christmas_tree_2023_qs8x12a0.75.jpg?v=1702708834115',
-    new THREE.Vector2(8, 12) // quilt col & row count
-  );
-  blockCard1.position.x = -0.2;
-  cardGroup.add(blockCard1);
-  
-  // Our Christmas Tree 2023 Close-up
-  let blockCard2 = createBlockCard(
-    "https://cdn.glitch.global/98b2b4e8-ce2c-4c4f-8e0c-3e762cb48276/closeup_qs8x12a0.75.png?v=1702865989253",
-    new THREE.Vector2(8, 7) // quilt col & row count
-  );
-  cardGroup.add(blockCard2);
-
-  // Jupiter
-  let blockCard3 = createBlockCard(
-    "https://lkg-blocks.imgix.net/u/052ce5cfe2ad4595/julpiterLGS_v002__qs8x6a0.75.png?ixlib=js-3.7.0&fm=webp&auto=format&fit=max&w=3360",
-    new THREE.Vector2(8, 6) // quilt col & row count
-  );
-  blockCard3.position.x = 0.2;
-  cardGroup.add(blockCard3);
-    
-  // Set the cards up as visible in the 2D view
-  cardGroup.position.z = -2.5;
-  camera.fov = 5;
-  camera.updateProjectionMatrix();
-  
-  // Setup camera controls for the 2D view
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.025;
-  controls.target.copy(cardGroup.position);
-  controls.update();
-
-  // Lock rotation around the X axis
-  controls.minPolarAngle = Math.PI / 2;
-  controls.maxPolarAngle = Math.PI / 2;
-
-  // Lock rotation around the Z axis
-  const angleLimit = (maxViewingAngle * Math.PI) / 180;
-  const halfAngleLimit = angleLimit / 2;
-  controls.minAzimuthAngle = -halfAngleLimit;
-  controls.maxAzimuthAngle = halfAngleLimit;
-
-  
-  // The main animation and render loop
-  function animate(timestamp, frame) {
-    if (renderer.xr.isPresenting) {
-      // In VR mode, highlight any card(s) being pointed at
-      cleanIntersected();
-
-      intersectObjects( controller1 );
-      intersectObjects( controller2 );
-    } else {
-      // In 2D mode, the orbit controls need this for damping to work
-      controls.update();
-    }
-    
-    // Render the scene on each frame!!
-    renderer.render(scene, camera);
-  }
-  
-  // Start the loop!!
-  renderer.setAnimationLoop(animate);
-  
-  
-});
-
-
-
-window.addEventListener("resize", onWindowResize, false);
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
 }
