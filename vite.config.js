fb75fa9ae19232344e891cf184648125a70dc71c
Brian R. Jackson <brian@jaxzin.com>
2024-01-01 13:39:28 -0500

    Add development HTTPS/SSL/TLS for testing (WebXR requires it).

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   build: {
     outDir: "build"
   },
@@ -14,5 +15,10 @@ export default defineConfig({
     hmr: {
       clientPort: 443 // Run the websocket server on the SSL port
     }
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+    https: true
   }
 });
